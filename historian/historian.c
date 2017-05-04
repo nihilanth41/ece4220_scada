278abcec5b6fcc3cdff9699209a84dca169a9cae
Shipman <twswwd@nfs1.ece.missouri.edu>
2017-05-04 13:47:23 -0500

    Struct->string and write to log file
    - Fix compile errors

--- historian/historian.c
+++ historian/historian.c
@@ -10,114 +10,118 @@
 #include <netinet/in.h>
 #include <netdb.h>
 #include <arpa/inet.h>
+#include <errno.h>
 #include "status.h"
 
 #define MSG_SIZE 150			// message size
 
-typedef struct {
-	int ID;
-	double timestamp; 			// Look at lab4 for formatting. Should be a timeval
-	int LEDs[2]; 				// = {false, false, false};
-	int buttons[3]; 			// = {false, false, false, false, false};
-	double voltage; 			//string because it was difficult to do a string to float conversion
-	char eventChange[MSG_SIZE]; //If an LED was turned on or off, which one? Same for buttons
-} Data;
+void error(const char *msg, int errval);
+int logEvent(status_t event); 
+double adc_to_volts(int adc_val);
+void event_to_string(char *buf, int event_type);
+void event_to_string(char *buf, int event_type) {
+	const char *event_strings[13] = { "Button 0 Falling Edge", 
+			   "Button 0 Rising Edge", 
+			   "Button 1 Falling Edge",
+			   "Button 1 Rising Edge", 
+			   "Button 2 Falling Edge",
+			   "Button 2 Rising Edge",
+			   "Line Overvoltage",
+			   "Line Undervoltage",
+			   "Line No Power",
+			   "LED 1 On",
+			   "LED 1 Off",
+			   "LED 2 On",
+			   "LED 2 Off" };
+	sprintf(buf, "%s", event_strings[event_type]);
+}
 
-Data rtu[3]; //stores up to 3 rtu data
-status_t event;
-char* tokens;
 
-void error(const char *);
+// Convert event struct to string and write to a file.
+// Gets filename from rtu_id.
+// Params: status_t event 
+// Return: 0 for success, nonzero for failure.
+int logEvent(status_t event) {
+	char filename[MSG_SIZE];
+	char log_str[MSG_SIZE];
+	char event_str[MSG_SIZE];
+	sprintf(filename, "rtu_%d_log.txt", event.rtu_id);
+	FILE* fp = fopen(filename, "a+");
+	if(NULL == fp) { 
+		return -1;
+	}
+	// rtu_id, time, btn0, btn1, btn2, led1, led2, voltage, event_string
+	int rtu_id = event.rtu_id;
+	double time = (double)event.event_tv.tv_sec + ((double)event.event_tv.tv_usec*(0.000001));
+	int btn0 = event.button_status[0];
+	int btn1 = event.button_status[1];
+	int btn2 = event.button_status[2];
+	int led1 = event.led_status[0];
+	int led2 = event.led_status[1];
+	double voltage = adc_to_volts(event.line_voltage);
+	char *pBuf = (char *)&event_str;
+	event_to_string(pBuf, event.event_type);
+	pBuf = (char *)&log_str;
+	sprintf(pBuf, "%d|%lf|%d|%d|%d|%d|%d|%lf|%s|\n", rtu_id, time, btn0, btn1, btn2, led1, led2, voltage, event_str);
+	fprintf(fp, "%s", log_str);
+	// debug
+	printf("Logging: %s", log_str);
+
+	fclose(fp);
+	return 0;
+}
+
 
 int main(int argc, char *argv[]) {
 	int sock, n;
 	unsigned int length;
 	struct sockaddr_in server, from;
 	struct hostent *hp;
-	char fileName[15];
 	char buffer[MSG_SIZE];
-	char buffer2[MSG_SIZE];
-	FILE *fp;
+	status_t event;
+	
 
 	strcpy(buffer, "HANDSHAKE");
 	if (argc != 3) {
-		printf("usage %s hostname port\n", argv[0]);
-		exit(1);
+		fprintf(stderr, "Usage: %s hostname port\n", argv[0]);
+		return -1;
 	}
 
 	sock = socket(AF_INET, SOCK_DGRAM, 0); // Creates socket. Connectionless.
-	if (sock < 0)
-		error("socket");
+	if (sock < 0) {
+		error("socket()", errno);
+	}
 
 	server.sin_family = AF_INET; // symbol constant for Internet domain
 	hp = gethostbyname(argv[1]); // converts hostname input (e.g. 10.3.52.15)
-	if (hp == 0)
-		error("Unknown host");
+	if (hp == 0) {
+		error("Unknown host", -1);
+	}
 
 	bcopy((char *) hp->h_addr, (char *) &server.sin_addr, hp->h_length);
 	server.sin_port = htons(atoi(argv[2])); // port field
 	length = sizeof(struct sockaddr_in); // size of structure
-	while (1) {
+
+	while(1) 
+	{
 
 		//printf("Please  enter the message: ");
-		bzero(buffer2, MSG_SIZE); // sets all values to zero. memset() could be used
+		bzero((void *)&event, sizeof(event)); // sets all values to zero. memset() could be used
 
-		n = sendto(sock, buffer, strlen(buffer), 0,
-				(const struct sockaddr *) &server, length);
+		if( sendto(sock, buffer, strlen(buffer), 0, (const struct sockaddr *) &server, length) < 0 ) {
+			error("sendto", errno); 
+		}
 
 		// receive from server
-		n = recvfrom(sock, buffer2, MSG_SIZE, 0, (struct sockaddr *) &from,
-				&length);
-		if (n < 0)
-			error("recvfrom");
-
-		//Parser needs to be fprintf(fp, "LED3: %d\n", rtu[1].LEDs[2]);put in a function
-		tokens = strtok(buffer2, "|");
-		rtu[1].ID = atoi(tokens);
-
-		snprintf(fileName, sizeof(char)*15, "Log%d.txt", rtu[1].ID);
-
-		fp = fopen(fileName, "ab+");	//Name using ID num from RTU?c d
-
-		//Start Parser
-		tokens = strtok(NULL, "|");
-		sscanf(tokens, "%lf", &rtu[1].timestamp);
-		tokens = strtok(NULL, "|");
-		rtu[1].LEDs[0] = atoi(tokens);
-		tokens = strtok(NULL, "|");
-		rtu[1].LEDs[1] = atoi(tokens);
-		tokens = strtok(NULL, "|");
-		rtu[1].buttons[0] = atoi(tokens);
-		tokens = strtok(NULL, "|");
-		rtu[1].buttons[1] = atoi(tokens);
-		tokens = strtok(NULL, "|");
-		rtu[1].buttons[2] = atoi(tokens);
-		tokens = strtok(NULL, "|");
-		sscanf(tokens, "%lf", &rtu[1].voltage);
-		//End parser
-
-		printf("ID: %d\n", rtu[1].ID);
-		printf("Time Stamp: %lf\n", rtu[1].timestamp);
-		printf("LED1: %d\n", rtu[1].LEDs[0]);
-		printf("LED2: %d\n", rtu[1].LEDs[1]);
-		printf("Button1: %d\n", rtu[1].buttons[0]);
-		printf("Button2: %d\n", rtu[1].buttons[1]);
-		printf("Button3: %d\n", rtu[1].buttons[2]);
-		printf("Voltage: %.3lf\n\n", rtu[1].voltage);
-
-		fprintf(fp, "ID: %d\n", rtu[1].ID);
-		fprintf(fp, "Time Stamp: %lf\n", rtu[1].timestamp);
-		fprintf(fp, "LED1: %d\n", rtu[1].LEDs[0]);
-		fprintf(fp, "LED2: %d\n", rtu[1].LEDs[1]);
-		fprintf(fp, "Button1: %d\n", rtu[1].buttons[0]);
-		fprintf(fp, "Button2: %d\n", rtu[1].buttons[1]);
-		fprintf(fp, "Button3: %d\n", rtu[1].buttons[2]);
-		fprintf(fp, "Voltage: %.3lf\n", rtu[1].voltage);
-		fprintf(fp, "\n");
-
-		fclose(fp);
+		n = recvfrom(sock,&event,sizeof(event), 0, (struct sockaddr *) &from, &length);
+		if (n < 0) {
+			error("recvfrom", errno);
+		}
+		else {
+			logEvent(event);
+		}
+		sleep(0);
 	}
-	fclose(fp);
 	close(sock); // close socket.
 	return 0;
 }
@@ -125,8 +129,18 @@ int main(int argc, char *argv[]) {
 //USE FOR SORTING. Put in function? sprintf based on current log files
 //system("cat Log1.txt Log2.txt | sort -t'|' -k3 -n";
 
+double adc_to_volts(int adc_val) {
+ const int num_bits = 4095; // 4096-1 (2^12-1)
+ const int vref = 5; // 5v
+ const double vpd = ( (double)vref / (double)num_bits );
+ return (double)adc_val * vpd;
+}
 
-void error(const char *msg) {
-	perror(msg);
-	exit(0);
+
+void error(const char *msg, int errval) {
+	if(NULL == msg) {
+		return;
+	}
+	fprintf(stderr, "Error: %s\n", msg);
+	exit(errval);
 }
