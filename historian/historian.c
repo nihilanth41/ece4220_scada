9a482b405230c8c38debd509287a10110f940685
Zachary Rump <zrrm74@mail.missouri.edu>
2017-05-05 15:17:59 -0500

    Add log & cat function

--- historian/historian.c
+++ historian/historian.c
@@ -13,64 +13,25 @@
 #include <errno.h>
 #include "status.h"
 
-#define MSG_SIZE 150			// message size
+#define MSG_SIZE 150			
 
-void error(const char *msg, int errval);
-int logEvent(status_t event); 
+/* Prototypes */
 double adc_to_volts(int adc_val);
+void error(const char *msg, int errval);
+int event_log(status_t event); 
 void event_to_string(char *buf, int event_type);
-void event_to_string(char *buf, int event_type) {
-	const char *event_strings[13] = { "Button 0 Falling Edge", 
-			   "Button 0 Rising Edge", 
-			   "Button 1 Falling Edge",
-			   "Button 1 Rising Edge", 
-			   "Button 2 Falling Edge",
-			   "Button 2 Rising Edge",
-			   "Line Overvoltage",
-			   "Line Undervoltage",
-			   "Line No Power",
-			   "LED 1 On",
-			   "LED 1 Off",
-			   "LED 2 On",
-			   "LED 2 Off" };
-	sprintf(buf, "%s", event_strings[event_type]);
+void log_sort(void);
+void log_print(void);
+
+// We only have to deal with two RTUs in this project 
+// rtu_1_log.txt and rtu_2_log.txt
+//system("cat Log1.txt Log2.txt | sort -t'|' -k3 -n";
+void log_view(void) {
+const char *command1 = "cat rtu_1_log.txt rtu_2_log.txt | sort -t'|' -k3 -n > event_log.txt";
+const char *command2 = "cat event_log.txt";
+system(command1);
+system(command2);
 }
-
-
-// Convert event struct to string and write to a file.
-// Gets filename from rtu_id.
-// Params: status_t event 
-// Return: 0 for success, nonzero for failure.
-int logEvent(status_t event) {
-	char filename[MSG_SIZE];
-	char log_str[MSG_SIZE];
-	char event_str[MSG_SIZE];
-	sprintf(filename, "rtu_%d_log.txt", event.rtu_id);
-	FILE* fp = fopen(filename, "a+");
-	if(NULL == fp) { 
-		return -1;
-	}
-	// rtu_id, time, btn0, btn1, btn2, led1, led2, voltage, event_string
-	int rtu_id = event.rtu_id;
-	double time = (double)event.event_tv.tv_sec + ((double)event.event_tv.tv_usec*(0.000001));
-	int btn0 = event.button_status[0];
-	int btn1 = event.button_status[1];
-	int btn2 = event.button_status[2];
-	int led1 = event.led_status[0];
-	int led2 = event.led_status[1];
-	double voltage = adc_to_volts(event.line_voltage);
-	char *pBuf = (char *)&event_str;
-	event_to_string(pBuf, event.event_type);
-	pBuf = (char *)&log_str;
-	sprintf(pBuf, "%d|%lf|%d|%d|%d|%d|%d|%lf|%s|\n", rtu_id, time, btn0, btn1, btn2, led1, led2, voltage, event_str);
-	fprintf(fp, "%s", log_str);
-	// debug
-	printf("Logging: %s", log_str);
-
-	fclose(fp);
-	return 0;
-}
-
 
 int main(int argc, char *argv[]) {
 	int sock, n;
@@ -118,7 +79,7 @@ int main(int argc, char *argv[]) {
 			error("recvfrom", errno);
 		}
 		else {
-			logEvent(event);
+			event_log(event);
 		}
 		sleep(0);
 	}
@@ -126,8 +87,59 @@ int main(int argc, char *argv[]) {
 	return 0;
 }
 
-//USE FOR SORTING. Put in function? sprintf based on current log files
-//system("cat Log1.txt Log2.txt | sort -t'|' -k3 -n";
+
+
+// Convert event struct to string and write to a file.
+// Gets filename from rtu_id.
+// Params: status_t event 
+// Return: 0 for success, nonzero for failure.
+int event_log(status_t event) {
+	char filename[MSG_SIZE];
+	char log_str[MSG_SIZE];
+	char event_str[MSG_SIZE];
+	sprintf(filename, "rtu_%d_log.txt", event.rtu_id);
+	FILE* fp = fopen(filename, "a+");
+	if(NULL == fp) { 
+		return -1;
+	}
+	// rtu_id, time, btn0, btn1, btn2, led1, led2, voltage, event_string
+	int rtu_id = event.rtu_id;
+	double time = (double)event.event_tv.tv_sec + ((double)event.event_tv.tv_usec*(0.000001));
+	int btn0 = event.button_status[0];
+	int btn1 = event.button_status[1];
+	int btn2 = event.button_status[2];
+	int led1 = event.led_status[0];
+	int led2 = event.led_status[1];
+	double voltage = adc_to_volts(event.line_voltage);
+	char *pBuf = (char *)&event_str;
+	event_to_string(pBuf, event.event_type);
+	pBuf = (char *)&log_str;
+	sprintf(pBuf, "%d|%lf|%d|%d|%d|%d|%d|%lf|%s|\n", rtu_id, time, btn0, btn1, btn2, led1, led2, voltage, event_str);
+	fprintf(fp, "%s", log_str);
+	// debug
+	printf("Logging: %s", log_str);
+
+	fclose(fp);
+	return 0;
+}
+
+// Put the event type string corresponding to the event_type integer in the buffer pointed to by buf.
+void event_to_string(char *buf, int event_type) {
+	const char *event_strings[13] = { "Button 0 Falling Edge", 
+			   "Button 0 Rising Edge", 
+			   "Button 1 Falling Edge",
+			   "Button 1 Rising Edge", 
+			   "Button 2 Falling Edge",
+			   "Button 2 Rising Edge",
+			   "Line Overvoltage",
+			   "Line Undervoltage",
+			   "Line No Power",
+			   "LED 1 On",
+			   "LED 1 Off",
+			   "LED 2 On",
+			   "LED 2 Off" };
+	sprintf(buf, "%s", event_strings[event_type]);
+}
 
 double adc_to_volts(int adc_val) {
  const int num_bits = 4095; // 4096-1 (2^12-1)
